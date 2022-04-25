c10dc8922691b33211b5b5b3751a3c8c34d28df2
Yuandono <mazicalm7@gmail.com>
2022-04-25 14:47:50 +0700

    first commit

---
+++ app/src/main/java/com/example/yuandono_181103475_mid/profil.java
@@ -0,0 +1,14 @@
+package com.example.yuandono_181103475_mid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class profil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil);
+    }
+}
